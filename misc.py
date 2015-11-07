916c9a9146ff4be0d1b8479c641aae14a1ffe9b6
minus <minus@mnus.de>
2015-11-07 21:48:57 +0100

    fixed update.py

--- misc.py
+++ misc.py
@@ -5,27 +5,29 @@ import flask
 from functools import wraps
 
 
-def with_pg_cursor(fn):
+def with_pg_cursor(*cur_args, **cur_kwargs):
 	"""
 	Injects an argument `cur` containing a postgres cursor into the function arguments. Autocommits.
 	"""
-	@wraps(fn)
-	def wrapper(*args, **kwargs):
-		if "cur" in kwargs:
-			raise TypeError("Function may not have a keyword argument named cur")
-		conn = config.postgres.getconn()
-		cur = conn.cursor()
-		kwargs["cur"] = cur
-		try:
-			ret = fn(*args, **kwargs)
-			conn.commit()
-			return ret
-		except:
-			conn.rollback()
-			raise
-		finally:
-			config.postgres.putconn(conn)
-	return wrapper
+	def decorator(fn):
+		@wraps(fn)
+		def wrapper(*args, **kwargs):
+			if "cur" in kwargs:
+				raise TypeError("Function may not have a keyword argument named cur")
+			conn = config.postgres.getconn()
+			cur = conn.cursor(*cur_args, **cur_kwargs)
+			kwargs["cur"] = cur
+			try:
+				ret = fn(*args, **kwargs)
+				conn.commit()
+				return ret
+			except:
+				conn.rollback()
+				raise
+			finally:
+				config.postgres.putconn(conn)
+		return wrapper
+	return decorator
 
 
 def json_api(fn):
--- update.py
+++ update.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import config
+from misc import with_pg_cursor
 from schema import ix
 
 import sys
@@ -8,38 +9,38 @@ import psycopg2.extras
 
 step_size = 1000
 
-def update(limit_path=None, limit_ids=None):
+@with_pg_cursor(cursor_factory=psycopg2.extras.DictCursor)
+def update(cur, limit_path=None, limit_ids=None):
 	with ix.writer() as writer:
-		with config.postgres.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-			extra_args = []
-			extra_query = ""
-			if limit_path:
-				limit_path = limit_path.replace("%", "\\%").replace("_", "\\_") + "%"
-				extra_query += " AND s.location LIKE %s"
-				extra_args.append(limit_path)
-			if limit_ids:
-				extra_query += " AND s.id IN %s"
-				extra_args.append(limit_ids)
+		extra_args = []
+		extra_query = ""
+		if limit_path:
+			limit_path = limit_path.replace("%", "\\%").replace("_", "\\_") + "%"
+			extra_query += " AND s.location LIKE %s"
+			extra_args.append(limit_path)
+		if limit_ids:
+			extra_query += " AND s.id IN %s"
+			extra_args.append(limit_ids)
 
-			cur.execute("""
-				SELECT s.id AS id, s.hash AS hash, s.location AS path,
-					s.title AS title, ar.name AS artist, al.name AS album,
-					s.length AS duration, s.status AS status,
-					coalesce(string_agg(t.name, ','), '') AS tags
-				FROM songs AS s
-				LEFT OUTER JOIN artists AS ar ON s.artist = ar.id
-				LEFT OUTER JOIN albums AS al ON s.album = al.id
-				LEFT OUTER JOIN taggings AS ts ON s.id = ts.song
-				LEFT OUTER JOIN tags AS t ON ts.tag = t.id
-				WHERE
-					s.status IN ('active', 'unlisted') {}
-				GROUP BY
-					s.id, s.hash, s.location, s.title, ar.name, al.name, s.length, s.status;""".format(extra_query),
-				extra_args
-			)
-			for row in cur:
-				row = {k: str(v) for k, v in row.items()}
-				writer.update_document(**row)
+		cur.execute("""
+			SELECT s.id AS id, s.hash AS hash, s.location AS path,
+				s.title AS title, ar.name AS artist, al.name AS album,
+				s.length AS duration, s.status AS status,
+				coalesce(string_agg(t.name, ','), '') AS tags
+			FROM songs AS s
+			LEFT OUTER JOIN artists AS ar ON s.artist = ar.id
+			LEFT OUTER JOIN albums AS al ON s.album = al.id
+			LEFT OUTER JOIN taggings AS ts ON s.id = ts.song
+			LEFT OUTER JOIN tags AS t ON ts.tag = t.id
+			WHERE
+				s.status IN ('active', 'unlisted') {}
+			GROUP BY
+				s.id, s.hash, s.location, s.title, ar.name, al.name, s.length, s.status;""".format(extra_query),
+			extra_args
+		)
+		for row in cur:
+			row = {k: str(v) for k, v in row.items()}
+			writer.update_document(**row)
 
 if __name__ == "__main__":
 	limit_path = None
