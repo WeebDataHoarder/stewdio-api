e85ef9cbdf782f8228112b68f4222bd457db4590
minus <minus@mnus.de>
2016-01-01 12:26:06 +0100

    updated tagging API
    - use hashes instead of song ids
    - create tags implicitly

--- misc.py
+++ misc.py
@@ -38,8 +38,8 @@ def json_api(fn):
 		if isinstance(ret, flask.Response):
 			return ret
 		if isinstance(ret, tuple):
-			ret = ret[0]
 			status = ret[1]
+			ret = ret[0]
 		return flask.Response(
 			json.dumps(ret),
 			status=status,
@@ -48,3 +48,21 @@ def json_api(fn):
 			}
 		)
 	return wrapper
+
+def song_hash2id(*keyword_names):
+	def decorator(fn):
+		@wraps(fn)
+		def wrapper(*args, **kwargs):
+			if "cur" not in kwargs:
+				raise RuntimeError("DB connection injection is required")
+			cur = kwargs["cur"]
+			for keyword_name in keyword_names:
+				if keyword_name in kwargs:
+					arg = kwargs[keyword_name]
+					int(arg, 16)  # sanity check
+					arg += "%"
+					cur.execute("SELECT id FROM songs WHERE hash ILIKE %s", (arg,))
+					kwargs[keyword_name] = cur.fetchone()[0]
+			return fn(*args, **kwargs)
+		return wrapper
+	return decorator
--- tagging.py
+++ tagging.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-from misc import with_pg_cursor, json_api
+from misc import with_pg_cursor, json_api, song_hash2id
 from update import update
 
 from flask import Blueprint
@@ -10,12 +10,15 @@ api = Blueprint("tagging", "tagging", url_prefix="/api/tag")
 
 @api.route("/<song_id>/add/<tag>")
 @with_pg_cursor()
+@song_hash2id("song_id")
 @json_api
 def add(cur, song_id, tag):
 	cur.execute("SELECT id FROM tags WHERE name = %s", (tag,))
 	res = cur.fetchone()
 	if not res:
-		return {"error": "tag does not exist"}, 404
+		cur.execute("INSERT INTO tags (name) VALUES (%s) RETURNING id", (tag,))
+		res = cur.fetchone()
+
 	try:
 		cur.execute("INSERT INTO taggings (song, tag) VALUES (%s, %s)", (song_id, res[0]))
 	except psycopg2.IntegrityError:
@@ -25,25 +28,15 @@ def add(cur, song_id, tag):
 
 @api.route("/<song_id>/remove/<tag>")
 @with_pg_cursor()
+@song_hash2id("song_id")
 @json_api
 def remove(cur, song_id, tag):
 	cur.execute("SELECT id FROM tags WHERE name = %s", (tag,))
 	res = cur.fetchone()
 	if not res:
 		return {"error": "tag does not exist"}, 404
-	try:
-		cur.execute("DELETE FROM taggings WHERE song = %s AND tag = %s", (song_id, res[0]))
-	except psycopg2.IntegrityError:
+	cur.execute("DELETE FROM taggings WHERE song = %s AND tag = %s", (song_id, res[0]))
+	if cur.rowcount == 0:
 		return {"removed": False}, 200
 	update(limit_ids=(song_id,))
 	return {"removed": True}, 201
-
-@api.route("/create/<tag>")
-@with_pg_cursor()
-@json_api
-def create(cur, tag):
-	try:
-		cur.execute("INSERT INTO tags (name) VALUES (%s)", (tag,))
-	except psycopg2.IntegrityError:
-		return {"created": False}, 200
-	return {"created": True}, 201
