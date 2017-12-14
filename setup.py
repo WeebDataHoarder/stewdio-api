f8c2e4a7f5dd6fa3718ef6280fdd2bd3ee668545
minus <minus@mnus.de>
2017-12-14 21:14:51 +0100

    Add PKGBUILD

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+import subprocess
+import glob
+import os
+
+ver = os.environ.get("PKGVER") or subprocess.run(['git', 'describe', '--tags'], stdout=subprocess.PIPE).stdout.decode().strip()
+
+with open('requirements.txt') as f:
+    reqs = [l.strip() for l in f]
+
+setup(
+    name = 'stewdio',
+    packages = [
+        'stewdio',
+        'stewdio.search',
+        ],
+    version = ver,
+    description = 'Stewdio radio controller',
+    author = 'minus',
+    author_email = 'minus@mnus.de',
+    url = 'https://git.sr.ht/~minus/stewdio-api',
+    install_requires = reqs,
+    license = 'MIT',
+    package_data={
+        'stewdio': [
+            'templates/*.html',
+        ] + [fn[len('stewdio/'):] for fn in glob.glob('stewdio/static/**', recursive=True)],
+    },
+)
