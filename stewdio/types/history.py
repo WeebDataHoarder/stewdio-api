f70e0cf4e7a583f018e2277015b7f2bec563a7d0
minus <minus@mnus.de>
2018-08-24 23:04:31 +0200

    Add missing default in history table

--- stewdio/types/history.py
+++ stewdio/types/history.py
@@ -5,5 +5,5 @@ from ..database import Base
 class History(Base):
     __tablename__ = "history"
     id = sa.Column(sa.BIGINT, primary_key=True)
-    play_time = sa.Column(sa.TIMESTAMP(timezone=True), nullable=False)
+    play_time = sa.Column(sa.TIMESTAMP(timezone=True), nullable=False, server_default=sa.func.now())
     data = sa.Column(sa.JSON(), nullable=False)
